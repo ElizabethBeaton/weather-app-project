bdaa6e11bba9fce95920f340745b76ba4f8a306d
ElizabethBeaton <elizabethbeaton23@outlook.com>
2023-02-20 23:15:22 +0000

    Used API to show current temperature

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let feelsElement = document.querySelector("#feels-like");
+  let humidElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  feelsElement.innerHTML = `${Math.round(response.data.main.feels_like)}˚C`;
+  humidElement.innerHTML = `${response.data.main.humidity}%`;
+  windElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
+}
+let apiKey = "65ec67aa1a3f45a2b8d746ee7b0ab4e3";
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
+axios.get(apiUrl).then(displayTemperature);
